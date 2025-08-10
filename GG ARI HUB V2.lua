4ff6584cd97a8cb97ad0f2e2b39077716240074f
ARIISETIAWAN20 <150575400+ARIISETIAWAN20@users.noreply.github.com>
2025-08-10 14:26:47 +0800

    Update GG ARI HUB V2.lua

--- GG ARI HUB V2.lua
+++ GG ARI HUB V2.lua
@@ -1,1 +1,9 @@
-# GG-ARI-HUB-
+local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ARIISETIAWAN20/OPM-ARI-HUB/main/ARI%20HUB%20V2.lua"))()
+end)
+
+if success then
+    print("ScAri.lua berhasil dijalankan!")
+else
+    warn("Gagal menjalankan ScAri.lua: ", err)
+end
